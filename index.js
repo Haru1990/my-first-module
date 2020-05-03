5967a7fecd0b281c572070cd87155ebe829fc099
dongfangxu <21668338@qq.com>
2020-05-03 19:45:45 +0800

    add lib

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-exports.showMsg = function () {
-  console.log('this is my first module');
-}
+import './plugins/css/hiprint.css';
+import './plugins/css/print-lock.css';
+import hiprint from './plugins/js/hiprint.bundle';
+
+console.log('hiprint: ', hiprint);
+// window.hiprint = hiprint;
+
+export default hiprint;
+
