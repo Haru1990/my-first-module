644f1cf872aa742dcc71bd1a5c8e600edf4d3bdc
dongfangxu <21668338@qq.com>
2020-05-04 14:47:58 +0800

    add hiwprint

--- plugins/js/hiprint.bundle.js
+++ plugins/js/hiprint.bundle.js
@@ -11,6 +11,9 @@
 import jQuery from 'jquery';
 import JsBarcode from 'jsbarcode';
 import QRCode from 'qrcodejs2';
+import hiwprint from 'jquery.hiwprint.js';
+
+hiwprint(jQuery);
 
 function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return !!right[Symbol.hasInstance](left); } else { return left instanceof right; } }
 
